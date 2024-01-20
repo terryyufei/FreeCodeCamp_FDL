9f92b434895ac406221e99a6f4f3b44d2101d1a0
Terry Wambui <shikuwambui@gmail.com>
2024-01-21 01:43:32 +0300

    react

---
+++ react/willmount.jsx
@@ -0,0 +1,16 @@
+// Use the Lifecycle Method componentWillMount
+
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  componentWillMount() {
+    // Change code below this line
+    console.log("Does this work?")
+
+    // Change code above this line
+  }
+  render() {
+    return <div />
+  }
+};
